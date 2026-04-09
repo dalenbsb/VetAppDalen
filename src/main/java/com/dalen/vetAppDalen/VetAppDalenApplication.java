96cb260d23b0f549a3249274b9548e6e8b8f0ea9
Dalembert Menezes Cruz <dalenbsb@gmail.com>
2026-04-09 15:33:30 -0300

    Add files via upload
    subindo projeto

---
+++ src/main/java/com/dalen/vetAppDalen/VetAppDalenApplication.java
@@ -0,0 +1,13 @@
+package com.dalen.vetAppDalen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAppDalenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetAppDalenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalen/vetAppDalen/VetAppDalenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalen.vetAppDalen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetAppDalenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
