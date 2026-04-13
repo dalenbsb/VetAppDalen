53b874ba8fba73ff1f744b562d0ed42c347d491e
dalenbsb <dalenbsb@gmail.com>
2026-04-13 11:26:07 -0300

    ajuste de texto.

--- src/main/java/com/dalen/vetAppDalen/testeapi/TesteApiController.java
+++ src/main/java/com/dalen/vetAppDalen/testeapi/TesteApiController.java
@@ -1,17 +1,18 @@
 package com.dalen.vetAppDalen.testeapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesteApiController {
 	
-	@Value("${spring.profiles.active}")
+	@Value("${spring.profiles.active:prod}")
 	private String profile;
 	
 	@GetMapping("/api/hello")
     public String hello() {
-        return "Hello World, Dalembert Menezes Cruz agora com Deploy automático (" + profile + ");
+        return "Hello World, Dalembert Menezes Cruz agora com Deploy automático (" + profile + ")";
     }
 
 }
